fee141b0e46c2a03324282b95d6a2f359cc639a6
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-12 14:09:46 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[3];
+        animals[0] = new Shark();
+        animals[1] = new Turtle();
+        animals[2] = new Eagle();
+
+        for (Animal animal : animals) {
+            if (animal instanceof Shark) {
+                System.out.println("_________________________________________________________");
+                Shark shark = (Shark) animal;
+                shark.attack();
+            } else if (animal instanceof Turtle) {
+                Turtle turtle = (Turtle) animal;
+                turtle.swim();
+            } else if (animal instanceof Eagle) {
+                Eagle eagle = (Eagle) animal;
+                eagle.fly();
+            }
+            animal.makeSound();
+            System.out.println("_________________________________________________________");
+            System.out.println();
+        }
+        Shark[] sharks = new Shark[1];
+        Turtle[] turtles = new Turtle[1];
+        Eagle[] eagles = new Eagle[1];
+
+        for (Animal animal : animals) {
+            if (animal instanceof Shark) {
+                sharks[0] = (Shark) animal;
+            } else if (animal instanceof Turtle) {
+                turtles[0] = (Turtle) animal;
+            } else if (animal instanceof Eagle) {
+                eagles[0] = (Eagle) animal;
+            }
+        }
+        System.out.println("*********************************************************");
+        sharks[0].attack();
+        turtles[0].swim();
+        eagles[0].fly();
+        System.out.println("*********************************************************");
+    }
+}
